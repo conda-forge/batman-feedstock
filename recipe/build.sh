d9a5f7eaedbb5c53cb955a59a85e491421161572
Pamphile Roy <roy@cerfacs.fr>
2018-03-20 10:19:18 +0100

    Wheel fname

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 
-curl https://pypi.python.org/packages/b4/5c/077f910632476281428fe254807952eb47ca78e720d059a46178c541e669/backports.tempfile-1.0-py2.py3-none-any.whl#md5=3d65f9f0682b03a3fe00492969886872 > backports.tempfile.whl
+curl https://pypi.python.org/packages/b4/5c/077f910632476281428fe254807952eb47ca78e720d059a46178c541e669/backports.tempfile-1.0-py2.py3-none-any.whl#md5=3d65f9f0682b03a3fe00492969886872 > backports.tempfile-1.0-py2.py3-none-any.whl
 
-pip install backports.tempfile.whl
+pip install backports.tempfile-1.0-py2.py3-none-any.whl
 
 python -m pip install --no-deps --ignore-installed .
